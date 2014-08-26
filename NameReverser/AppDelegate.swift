8ca8b5fc33deef6f83202e44a0ef27fc9a17fd2a
robbystoned <robbystoned@gmail.com>
2014-08-26 23:17:29 +0200

    updates for xCode 6 beta 6 release
    had to change a boatload of stuff to get it compiling again in xcode 6
    beta 6

--- NameReverser/AppDelegate.swift
+++ NameReverser/AppDelegate.swift
@@ -28,17 +28,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         myFiledialog.canChooseDirectories = false
         myFiledialog.runModal()
         var chosenfile = myFiledialog.URL // holds path to selected file, if there is one
-        if(chosenfile){
+        if((chosenfile) != nil){
             textField.stringValue = chosenfile.path;
             var filename:String = chosenfile.lastPathComponent;
             var lastPeriod = filename.lastIndexOf(".");
             var name = filename.subString(0, length: lastPeriod);
-            var lastSlash = chosenfile.path.lastIndexOf("/");
-            var directory = chosenfile.path.subString(0, length: lastSlash)
+            var lastSlash = chosenfile.path?.lastIndexOf("/");
+            var directory = chosenfile.path?.subString(0, length: lastSlash!)
             var fileExtension = filename.subString(lastPeriod, length: (filename.length - lastPeriod));
-            
-            
-            println(directory + "/" + reverseFileName(name) + fileExtension);
+            println(directory! + "/" + reverseFileName(name) + fileExtension);
             if(fileExtension == ".mkv") {
                 let fileManager = NSFileManager.defaultManager();
               
@@ -68,7 +66,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         
         for var index = strArray.count - 1; index >= 0;--index {
-            reversedStr += strArray[index]
+            reversedStr.append(strArray[index])
         }
         
         return reversedStr;
